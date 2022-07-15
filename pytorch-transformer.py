8df04f9673b01a0122ba9f14f1b3baf9c537b4cc
Luke A Sargen <lukeasargen@gmail.com>
2022-07-14 22:35:42 -0400

    add test lm head

--- pytorch-transformer.py
+++ pytorch-transformer.py
@@ -1,9 +1,12 @@
 from collections import OrderedDict
+import time
 
 from einops import rearrange
 import torch
 from torch import nn
 from torch.nn import functional as F
+
+from torchsummary import summary
 
 class MSA(torch.nn.Module):
     def __init__(self, embd, dim, heads, dropout=0):
@@ -70,6 +73,7 @@ class ResidualAttentionBlock(torch.nn.Module):
 class Transformer(torch.nn.Module):
     def __init__(self, layers, embd, dim, heads=0, ff_multi=0, dropout=0):
         super(Transformer, self).__init__()
+        self.embd = embd
         self.blocks = nn.ModuleList([ResidualAttentionBlock(embd, dim, heads, ff_multi, dropout) for _ in range(layers)])
 
     def forward(self, x, mask=None):
@@ -77,38 +81,101 @@ class Transformer(torch.nn.Module):
             x = block(x, mask)
         return x
 
+class SimpleLM(nn.Module):
+    def __init__(self, transformer, vocab_size, vocab_dim, tie_weights=False):
+        super().__init__()
+        self.transformer = transformer
+        self.embedding = nn.Sequential()
+        self.embedding.add_module("embedding", nn.Embedding(num_embeddings=vocab_size, embedding_dim=vocab_dim))
+        self.head = nn.Sequential()
+        if transformer.embd != vocab_dim:
+            self.embedding.add_module("enc_proj", nn.Linear(vocab_dim, transformer.embd, bias=False))
+            self.head.add_module("dec_proj", nn.Linear(transformer.embd, vocab_dim, bias=False))
+        self.head.add_module("out", nn.Linear(vocab_dim, vocab_size, bias=False))
+
+        if tie_weights:
+            for idx, (enc, dec)  in enumerate(zip(self.embedding, reversed(self.head))):
+                if idx==0:
+                    dec.weight = enc.weight
+                else:
+                    dec.weight.data = enc.weight.data.T
+
+    def forward(self, token_ids, mask=None):
+        x = self.embedding(token_ids)
+        x = transformer(x, mask)
+        logits = self.head(x)
+        return logits
+
 if __name__ == "__main__":
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    gen_casual_mask = lambda x: torch.tril(torch.ones(x.shape[1], x.shape[1]))
 
     batch = 1
     tokens = 4
 
-    layers = 8
-    embd = 512
-    dim = 64
-    heads = 8
+    layers = 6
+    embd = 384
+    dim = 32
+    heads = 12
     ff_multi = 4
     dropout = 0.1
     
-    x = torch.rand(batch, tokens, embd)
-    print(f"{x.shape=}")
+    # x = torch.rand(batch, tokens, embd)
+    # print(f"{x.shape=}")
+    # mask = gen_casual_mask(x)
+    # mask = torch.rand(tokens, tokens) < 0.75
+    # print(f"{mask=}")
 
-    gen_casual_mask = lambda x: torch.tril(torch.ones(x.shape[1], x.shape[1]))
-    mask = gen_casual_mask(x)
-    mask = torch.rand(tokens, tokens) < 0.75
-    print(f"{mask=}")
-
-    msa = MSA(embd, dim, heads, dropout)
-    y_msa = msa(x, mask)
-    print(f"{y_msa.shape=}")
+    # msa = MSA(embd, dim, heads, dropout)
+    # y_msa = msa(x, mask)
+    # print(f"{y_msa.shape=}")
 
     # block = ResidualAttentionBlock(embd, dim, heads, ff_multi, dropout)
     # y_block = block(x, mask)
     # print(f"{y_block.shape=}")
 
-    # transformer = Transformer(layers, embd, dim, heads, ff_multi, dropout)
+    transformer = Transformer(layers, embd, dim, heads, ff_multi, dropout)
     # y_transformer = transformer(x, mask)
     # print(f"{y_transformer.shape=}")
+    # summary(transformer, input_size=(1, embd), device="cpu")
 
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # from torchsummary import summary
-    # summary(transformer.to(device), input_size=(1, embd))
+    vocab_size = 50257
+    vocab_dim = 64
+    tie_weights = True
+
+    token_ids = torch.randint(low=0, high=vocab_size, size=(1, 512)).to(device)
+    lm = SimpleLM(transformer, vocab_size, vocab_dim, tie_weights)
+    # logits = lm(token_ids)
+    # print(f"{logits.shape=}")
+
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    vocab_dims = [32, 64, 128, 256, 384, 512, 768, 1024]
+    with torch.no_grad():
+        for vocab_dim in vocab_dims:
+            params = vocab_dim*transformer.embd if transformer.embd!=vocab_dim else 0
+            params += vocab_dim*vocab_size
+            lm = SimpleLM(transformer, vocab_size, vocab_dim, tie_weights).to(device)
+            for i in range(10):
+                out = lm(token_ids)
+            n_iter = 35
+            start = time.time()
+            for i in range(n_iter):
+                out = lm(token_ids)
+            dt = time.time() - start
+            print(f"{vocab_dim=}. embed params={params/1e6:.1f}M. {dt/n_iter * 1e3:.3f}ms.")
+
+    # n_iter = 8
+    # lm = lm.to(device)
+    # for b in range(1, 1024, 1):
+    #     x = torch.randint(low=0, high=vocab_size, size=(b, 512), device=device)
+    #     torch.cuda.synchronize()
+    #     start = time.time()
+    #     for _ in range(n_iter):
+    #         lm(x)
+    #     torch.cuda.synchronize()
+    #     dt = time.time() - start
+    #     throughput = b * n_iter / dt
+    #     print(f"Batch: {b} \t {throughput:6.2f} samples/sec")
+    #     del x
+    #     torch.cuda.empty_cache()
